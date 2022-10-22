be4be7c7729db86dc6e0910c513362d17fac993d
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2022-10-22 15:36:25 -0400

    code

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-
-
-
 const baseUrl = "https://rickandmortyapi.com/api"
+
+
+function search (character, location, episode){
+    url1 = `${baseUrl}character/?name=${character}`console.log(url1)
+
+    url2 = `${baseUrl}location/?name=${location}`
+    console.log(url2)
+
+    url3 = `${baseUrl}episode/?name=${episode}`
+    console.log(url3)
+}
+
+search(character, location, episode)
