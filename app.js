b3021e3e9b3c4682566b4f056676a6edff7a200a
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2022-10-26 17:03:40 -0400

    changes

--- app.js
+++ app.js
@@ -20,9 +20,28 @@ $(function(){
         characters.forEach((character) => {
             const $h1 = $(`<h3>${character.name}, ${character.species}, ${character.status}</h3>`)   
             const $img = $(`<img src='' alt=${character.name} />`)
+
             $('body').append($h1)
+            
+            const $divCharacter = $("div.name")
+            $divCharacter.empty()
+            const $divSpecies = $("div.species")
+            $divSpecies.empty()
+            const $divStatus = $("Dead or Alive")
+            $divStatus.empty()
+            
+            $divCharacter.append( `Name: ${character.name}`)
+            $divSpecies.append( ` Species: ${character.species}`)
+            $divStatus.append( ` Dead or Alive: ${character.status}`)
         })   
     }
 
+})
+
+$("input[type=submit]").on("click", (event) => {
+    event.preventDefault()
+
+    const inputText = $("input[type=text]").val()
+    character(textInput)
 })
 
