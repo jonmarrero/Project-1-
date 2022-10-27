d916d23518a848c51a3b4fe42031d197d917aab2
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2022-10-27 18:24:23 -0400

    code6

--- app.js
+++ app.js
@@ -1,42 +1,60 @@
 const baseUrl = "https://rickandmortyapi.com/api"
 
-// $(function(){
+$(function(){
 
-//     const promise = $.ajax({
-//         url: 'https://rickandmortyapi.com/api/character'
-//     });
+    const promise = $.ajax({
+        url: 'https://rickandmortyapi.com/api/character'
+    });
 
-//     promise.then(
-//        (data) => {
-//         console.log(data)
-//         render(data.results)
-//        },
-//        (error) => {
-//         console.log(error)
-//        } 
-//     )
+    promise.then(
+       (data) => {
+        console.log(data)
+        render(data.results)
+       },
+       (error) => {
+        console.log(error)
+       } 
+    )
 
-//     function render(characters){
-//         characters.forEach((character) => {
-//             const $h1 = $(`<h3>${character.name}, ${character.species}, ${character.status}</h3>`)   
-//             const $img = $(`<img src='' alt=${character.name} />`)
+    function render(characters){
+        characters.forEach((character) => {
+            const $h1 = $(`<h2>${character.name}, ${character.species}, ${character.status}</h2>`)   
+            const $img = $(`<img src='' alt=${character.name} />`)
 
-//             $('body').append($h1)
+            $('body').append($h1)
             
-//             const $divCharacter = $("div.name")
-//             $divCharacter.empty()
-//             const $divSpecies = $("div.species")
-//             $divSpecies.empty()
-//             const $divStatus = $("Dead or Alive")
-//             $divStatus.empty()
+            const $divCharacter = $("div.name")
+            $divCharacter.empty()
+            const $divSpecies = $("div.species")
+            $divSpecies.empty()
+            const $divStatus = $("Dead or Alive")
+            $divStatus.empty()
             
-//             $divCharacter.append( `Name: ${character.name}`)
-//             $divSpecies.append( ` Species: ${character.species}`)
-//             $divStatus.append( ` Dead or Alive: ${character.status}`)
-//         })   
-//     }
+            $divCharacter.append( `Name: ${character.name}`)
+            $divSpecies.append( ` Species: ${character.species}`)
+            $divStatus.append( ` Dead or Alive: ${character.status}`)
+        })   
+    }
 
-// })
+    // function render(location) {
+    // location.forEach((location) => {
+    //     const $h1 = $(`<h3>${location.name}, ${character.type}, ${character.dimension}</h3>`)   
+    //     const $img = $(`<img src='' alt=${location.name} />`)
+
+    //     $('body').append($h1)
+        
+    //     const $divCharacter = $("div.name")
+    //     $divCharacter.empty()
+    //     const $divSpecies = $("div.species")
+    //     $divSpecies.empty()
+    //     const $divStatus = $("Dead or Alive")
+    //     $divStatus.empty()
+        
+    //     $divCharacter.append( `Name: ${character.name}`)
+    //     $divSpecies.append( ` Species: ${character.species}`)
+    //     $divStatus.append( ` Dead or Alive: ${character.status}`)
+    // })
+})
 
 
 // function characterSearch(character){
