ca97644dcab82a5409024c9201751c637cb57cee
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2022-10-27 17:25:00 -0400

    sampledata

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 const baseUrl = "https://rickandmortyapi.com/api"
 
-$(function(){
+// $(function(){
 
-    const promise = $.ajax({
-        url: 'https://rickandmortyapi.com/api/character'
-    });
+//     const promise = $.ajax({
+//         url: 'https://rickandmortyapi.com/api/character'
+//     });
 
-    promise.then(
-       (data) => {
-        console.log(data)
-        render(data.results)
-       },
-       (error) => {
-        console.log(error)
-       } 
-    )
+//     promise.then(
+//        (data) => {
+//         console.log(data)
+//         render(data.results)
+//        },
+//        (error) => {
+//         console.log(error)
+//        } 
+//     )
 
-    function render(characters){
-        characters.forEach((character) => {
-            const $h1 = $(`<h3>${character.name}, ${character.species}, ${character.status}</h3>`)   
-            const $img = $(`<img src='' alt=${character.name} />`)
+//     function render(characters){
+//         characters.forEach((character) => {
+//             const $h1 = $(`<h3>${character.name}, ${character.species}, ${character.status}</h3>`)   
+//             const $img = $(`<img src='' alt=${character.name} />`)
 
-            $('body').append($h1)
+//             $('body').append($h1)
             
-            const $divCharacter = $("div.name")
-            $divCharacter.empty()
-            const $divSpecies = $("div.species")
-            $divSpecies.empty()
-            const $divStatus = $("Dead or Alive")
-            $divStatus.empty()
+//             const $divCharacter = $("div.name")
+//             $divCharacter.empty()
+//             const $divSpecies = $("div.species")
+//             $divSpecies.empty()
+//             const $divStatus = $("Dead or Alive")
+//             $divStatus.empty()
             
-            $divCharacter.append( `Name: ${character.name}`)
-            $divSpecies.append( ` Species: ${character.species}`)
-            $divStatus.append( ` Dead or Alive: ${character.status}`)
-        })   
-    }
+//             $divCharacter.append( `Name: ${character.name}`)
+//             $divSpecies.append( ` Species: ${character.species}`)
+//             $divStatus.append( ` Dead or Alive: ${character.status}`)
+//         })   
+//     }
 
-})
+// })
 
 
 // function characterSearch(character){
@@ -62,11 +62,35 @@ $(function(){
 // })
 
 
-    $("input[type=submit]").on("click", (event) => {
-    event.preventDefault()
+//     $("input[type=submit]").on("click", (event) => {
+//     event.preventDefault()
 
-    const inputText = $("input[type=text]").val()
-    character(textInput)
-    characterSearch(inputText)
+//     const inputText = $("input[type=text]").val()
+//     character(textInput)
+//     characterSearch(inputText)
+// })
+
+
+$.ajax("https://rickandmortyapi.com/api")
+.then((data) => {
+    console.log(data)
 })
 
+$.ajax("https://rickandmortyapi.com/api/character")
+.then((data) => {
+    console.log(data)
+    console.log(data.results)
+})
+
+
+$.ajax("https://rickandmortyapi.com/api/location")
+.then((data) => {
+    console.log(data)
+    console.log(data.results)
+})
+
+$.ajax("https://rickandmortyapi.com/api/episode")
+.then((data) => {
+    console.log(data)
+    console.log(data.results)
+})
